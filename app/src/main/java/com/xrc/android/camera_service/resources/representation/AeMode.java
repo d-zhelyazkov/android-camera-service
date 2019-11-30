db4ef377637bde9f43e6e0930e72111c4321f512
dzhelyazkov <d.zhelyazkov@ocado.com>
2019-11-30 23:24:09 +0200

    AE Mode representation introduced

---
+++ app/src/main/java/com/xrc/android/camera_service/resources/representation/AeMode.java
@@ -0,0 +1,6 @@
+package com.xrc.android.camera_service.resources.representation;
+
+public enum AeMode {
+    OFF,
+    ON
+}
---
+++ app/src/main/java/com/xrc/android/camera_service/resources/representation/AeModeValue.java
@@ -0,0 +1,4 @@
+package com.xrc.android.camera_service.resources.representation;
+
+public class AeModeValue extends ValueBase<AeMode> {
+}
