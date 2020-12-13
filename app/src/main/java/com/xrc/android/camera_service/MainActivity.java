f4cfab15eee9b41ad5bf493379d78d51df618f70
DZhelyazkov <d.zhelyazkov@ocado.com>
2020-12-13 19:00:01 +0200

    Remove capture toast

--- app/src/main/java/com/xrc/android/camera_service/MainActivity.java
+++ app/src/main/java/com/xrc/android/camera_service/MainActivity.java
@@ -1,13 +1,10 @@
 package com.xrc.android.camera_service;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
-import io.reactivex.Observable;
 
 public class MainActivity extends Activity {
 
@@ -42,7 +39,6 @@ public class MainActivity extends Activity {
                 cameraController.startPreview();
                 server.start();
 
-                setupCaptureToast();
                 settingsDisplay.start();
 
             } catch (Exception e) {
@@ -74,16 +70,6 @@ public class MainActivity extends Activity {
                 throw new RuntimeException(e);
             }
         });
-    }
-
-    @SuppressLint("CheckResult")
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void setupCaptureToast() {
-        String captureMsg = getString(R.string.capture_msg);
-        Toast toast = Toast.makeText(this, captureMsg, Toast.LENGTH_SHORT);
-
-        Observable<byte[]> captureImageObservable = cameraController.getCaptureImageObservable();
-        captureImageObservable.subscribe(bytes -> toast.show());
     }
 
 }
